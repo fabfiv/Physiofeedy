aa0eb8be46263428104f9423cdb56522d8e34ee7
fabfiv <fablusfve@gmail.com>
2025-03-21 01:03:51 +0530

    Added CORS and environment variable support

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to PhysioFeedy API!"})
+    return jsonify({"message": "Welcome to PhysioFeedy API!"}) 
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
