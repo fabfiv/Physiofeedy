58d9d03771d919c8253cc77975e3bc1cbf7f7375
fabfiv <fablusfve@gmail.com>
2025-03-21 15:18:54 +0530

    Added video streaming with OpenCV

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+import cv2
 
 load_dotenv()
 
@@ -10,9 +11,31 @@ CORS(app)
 
 
 
+
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()  # Read a frame
+        if not success:
+            break
+        else:
+            # Encode frame as JPEG
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Yield frame for streaming
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to PhysioFeedy API!"}) 
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
