8387d95b62dd1570c36465c0de185dfdd814dcf5
girishshirsath <girishshirsath2004@gmail.com>
2025-06-10 17:50:22 +0530

    add final chnage from girish

--- Backend/server.py
+++ Backend/server.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, Response, request, Request
-#from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, decode_token
 from models import db, User, ExerciseHistory, BlacklistedToken
